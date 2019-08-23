4222d81df6a8fa5c33b92df2a9f27fcd7dfd1998
Pablo Alberto Espinoza Ruiz <pabloalber84@gmail.com>
2019-08-23 16:05:39 -0700

    Hacer que ingrese los datos de cada usuario.
    Creado procedimiento; hace que ingrese los datos de cada usuario para generar registrar y generar su ID.
    Creada función "Continuar".

--- usuarios/Program.cs
+++ usuarios/Program.cs
@@ -8,8 +8,8 @@ namespace usuarios
         public string usuario;
         public string clave;
         public string nombres;
-        public string apellido_m;
         public string apellido_p;
+        public string apellido_m;
         public int edad;
         // Constructor
         public Usuario() {
@@ -27,6 +27,45 @@ namespace usuarios
             Console.WriteLine("¿Cuantos usuarios tendra su base de datos?");
             int size = int.Parse(Console.ReadLine());
             database = new Usuario[size];
+            IngresarDatosDB();
+        }
+		/*
+		 * Procedimiento IngresarDatosDB: Ingresa los datos de la cantidad de usuarios a ingresar
+		*/
+        static void IngresarDatosDB() {
+            for(int i = 0; i < database.Length; i++) {
+                Usuario base_usuario = new Usuario();
+                Console.WriteLine("|----------------------------------------|");
+                Console.Write("[#{0}] Ingrese el usuario: ", i);
+                base_usuario.usuario = Console.ReadLine();
+                Console.Write("[#{0}] Ingrese la contraseña: ", i);
+                base_usuario.clave = Console.ReadLine();
+                Console.Write("[#{0}] Ingrese los nombres: ", i);
+                base_usuario.nombres = Console.ReadLine();
+                Console.Write("[#{0}] Ingrese el apellido paterno: ", i);
+                base_usuario.apellido_p = Console.ReadLine();
+                Console.Write("[#{0}] Ingrese el apellido materno: ", i);
+                base_usuario.apellido_m = Console.ReadLine();
+                Console.Write("[#{0}] Ingrese la edad: ", i);
+                base_usuario.edad = int.Parse(Console.ReadLine());
+                Console.WriteLine("\n Usuario [#{0}] '{1}' se ha generado con el ID: {2}", i, base_usuario.usuario, base_usuario.id);
+                Console.WriteLine("|----------------------------------------|");
+            }
+            Continuar(true);
+        }
+        /*
+         * Funcion Continuar(bool del): Pide que ingrese al usuario que ingrese cualquier tecla para continuar,
+         *                              en caso que se requiera que el usuario alcance a ver algo importante
+         *                              antes de poder continuar con el proceso.
+         *                  PARAMETROS:
+         *                         bool del:    Es opcional, si se pasa 'true' como parametro, borrara el log
+         *                                      de la consola despues de oprimir cualquier boton.
+        */
+        static void Continuar(bool del = false) {
+            Console.Write("Ingrese cualquier tecla para continuar: ");
+            Console.ReadKey();
+            if(del)
+                Console.Clear();
         }
     }
 }
