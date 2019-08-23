0519ff781fd341d8dfdacae6c9d528464d6c2172
Pablo Alberto Espinoza Ruiz <pabloalber84@gmail.com>
2019-08-23 16:22:47 -0700

    Arreglar Syntax y agregar Menu de Opciones
    Arreglar sintaxis en el codigo (opcionales) y agrega el void "MenuOpciones" para manejar el control de datos de los usuarios.

--- usuarios/Program.cs
+++ usuarios/Program.cs
@@ -28,12 +28,18 @@ namespace usuarios
             int size = int.Parse(Console.ReadLine());
             database = new Usuario[size];
             IngresarDatosDB();
+            MenuOpciones();
         }
 		/*
-		 * Procedimiento IngresarDatosDB: Ingresa los datos de la cantidad de usuarios a ingresar
+		 * Procedimiento IngresarDatosDB()
+         *
+         * Ingresa los datos de la cantidad de usuarios a ingresar
+         *
 		*/
-        static void IngresarDatosDB() {
-            for(int i = 0; i < database.Length; i++) {
+        static void IngresarDatosDB()
+        {
+            for(int i = 0; i < database.Length; i++)
+            {
                 Usuario base_usuario = new Usuario();
                 Console.WriteLine("|----------------------------------------|");
                 Console.Write("[#{0}] Ingrese el usuario: ", i);
@@ -54,18 +60,64 @@ namespace usuarios
             Continuar(true);
         }
         /*
-         * Funcion Continuar(bool del): Pide que ingrese al usuario que ingrese cualquier tecla para continuar,
-         *                              en caso que se requiera que el usuario alcance a ver algo importante
-         *                              antes de poder continuar con el proceso.
-         *                  PARAMETROS:
-         *                         bool del:    Es opcional, si se pasa 'true' como parametro, borrara el log
-         *                                      de la consola despues de oprimir cualquier boton.
+         * Procedimiento Continuar(bool del)
+         *
+         * Pide que ingrese al usuario que ingrese cualquier tecla para continuar, en caso que
+         * se requiera que el usuario alcance a ver algo importante antes de poder continuar con
+         * el proceso.
+         *
+         * PARAMETROS:
+         *            bool del:    Es opcional, si se pasa 'true' como parametro, borrara el log
+         *                         de la consola despues de oprimir cualquier boton.
+         *
         */
-        static void Continuar(bool del = false) {
+        static void Continuar(bool del = false)
+        {
             Console.Write("Ingrese cualquier tecla para continuar: ");
             Console.ReadKey();
             if(del)
                 Console.Clear();
+        }
+		/*
+		 * Procedimiento MenuOpciones()
+         *
+         * Muestra las opciones que tiene para manejar la base de datos de los usuarios.
+         *
+		*/
+        static void MenuOpciones()
+        {
+            // Referencia para salto de codigo
+            BackMenu:
+            Console.WriteLine("|--------------------------------------|");
+            Console.WriteLine("|-          MENU DE OPCIONES          -|");
+            Console.WriteLine("|-         Ingrese una opción         -|");
+            Console.WriteLine("|--------------------------------------|");
+            Console.WriteLine("| 1.- Buscar Usuario                  -|");
+            Console.WriteLine("| 2.- Crear usuario                   -|");
+            Console.WriteLine("| 3.- Modificar Usuario               -|");
+            Console.WriteLine("| 4.- Borrar usuario                  -|");
+            Console.WriteLine("| 5.- Salir                           -|");
+            Console.WriteLine("|--------------------------------------|");
+            int opcion = int.Parse(Console.ReadLine());
+            switch(opcion)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    Environment.Exit(1);
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Opción invalida.");
+                    // Salto de codigo a la referencia "BackMenu"
+                    goto BackMenu;
+            }
         }
     }
 }
