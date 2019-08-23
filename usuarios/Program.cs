7acf64ec1702f7b39a91d06c69c96115e459bb1d
Pablo Alberto Espinoza Ruiz <pabloalber84@gmail.com>
2019-08-23 15:31:16 -0700

    Crear Clase "Usuario"
    Crear la clase Usuario con 7 variables y 1 constructor que genera automaticamente una ID tipo GUID al invocar la clase y generar un objeto

--- usuarios/Program.cs
+++ usuarios/Program.cs
@@ -2,6 +2,21 @@
 
 namespace usuarios
 {
+    class Usuario
+    {
+        public string id;
+        public string usuario;
+        public string clave;
+        public string nombres;
+        public string apellido_m;
+        public string apellido_p;
+        public int edad;
+        // Constructor
+        public Usuario() {
+            // Generar ID automaticamente al crear objeto
+            id = System.Guid.NewGuid().ToString();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
