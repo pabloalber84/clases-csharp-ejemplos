807f0529d1a6a8255712357bfa29e683a06274cb
Pablo Alberto Espinoza Ruiz <pabloalber84@gmail.com>
2019-09-01 12:42:05 -0700

    Create class Bici

--- bici/Program.cs
+++ bici/Program.cs
@@ -2,11 +2,28 @@
 
 namespace bici
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Bici
+	{
+		public string model;
+		public int velocities;
+		private int current_velocity;
+		public void init(string m, int ve, int cv) {
+			model = m;
+			velocities = ve;
+			if(cv > ve)
+				current_velocity = ve;
+			else
+				current_velocity = cv;
+		}
+		public void print() {
+			Console.WriteLine("Modelo: {0}\nVelocidad: {1}", model, current_velocity);
+		}
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
 }
